49b649e30563a8df538f8c5832d537c969795692
Praveentelukala <praveentelukala2003@gmail.com>
2026-04-15 18:07:55 +0530

    added home route

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ async function sendMessage() {
 
   chatbox.scrollTop = chatbox.scrollHeight;
 
-  const res = await fetch("http://localhost:5000/chat", {
+  const res = await fetch("https://coffeeshop-chatbot.onrender.com/chat", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ message }),
